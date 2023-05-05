015d9577a9b300f2bc8acff2b6d094349e139043
FlorianBeiser <florian.beiser@sintef.no>
2023-05-05 15:09:26 +0200

    generating ensemble with KL error in time

--- utils/BasinEnsembleInit.py
+++ utils/BasinEnsembleInit.py
@@ -2,41 +2,77 @@ from utils.BasinInit import *
 
 from gpuocean.SWEsimulators import CDKLM16, ModelErrorKL
 
-def initMLensemble(ls, ML_Nes, model_error_args_list, model_error_basis_args, sample_args):
+def initMLensemble(ML_Nes, args_list, make_data_args, sample_args, 
+                   init_model_error_basis_args=None, sim_model_error_basis_args=None, sim_model_error_time_step=None):
+    """
+    Utility function for the generation of multi-level ensembles of specific kind
+
+    ML_Nes - list with ensemble sizes per level
+    args_list - list of dicts with grid details per level, required keys per level: ny, ny, dx, dy, gpu_ctx, gpu_stream, boundary_condtions
+    make_data_args - function that takes one element of args_list and returns the initial fields (see e.g. `BasinInit.py`), required output keys: eta0, hu0, hv0, Hi
+    sample_args - dict with additional arguments to the Simulator
+    init_model_error_basis_args - dict with arguments for ModelErrorKL-class used for initial perturbations. `None` means no initial model error
+    sim_model_error_basis_args - dict with arguments for ModelErrorKL-class used for temporal perturbations. `None` means no temporal model error
+    sim_model_error_time_step - float>0 as CDKLM16.model_time_step (aka. the intervals in which sim_model_error is applied)
+    """
+
+    assert len(ML_Nes) == len(args_list), "Number of levels in data_args and level sizes do not match"
 
     # Model errors
-    mekls = []
-    for l_idx in range(len(ls)): 
-        mekls.append( ModelErrorKL.ModelErrorKL(**model_error_args_list[l_idx], **model_error_basis_args) )
+    if init_model_error_basis_args: 
+        init_mekls = []
+        for l_idx in range(len(args_list)): 
+            init_mekls.append( ModelErrorKL.ModelErrorKL(**args_list[l_idx], **init_model_error_basis_args) )
+
+    if sim_model_error_basis_args: 
+        sim_mekls = []
+        for l_idx in range(len(args_list)): 
+            sim_mekls.append( ModelErrorKL.ModelErrorKL(**args_list[l_idx], **sim_model_error_basis_args) )
+
+
 
     ## MultiLevel ensemble
     ML_ensemble = []
 
     # 0-level
     lvl_ensemble = []
-    data_args = make_init_fields(model_error_args_list[0])
+    data_args = make_data_args(args_list[0])
     for i in range(ML_Nes[0]):
         if i % 100 == 0: print(i)
-        sim = make_sim(model_error_args_list[0], sample_args, init_fields=data_args)
-        mekls[0].perturbSim(sim)
+        sim = make_sim(args_list[0], sample_args, init_fields=data_args)
+        if init_model_error_basis_args:
+            init_mekls[0].perturbSim(sim)
+        if sim_model_error_basis_args:
+            # sim.setKLModelError(**sim_model_error_basis_args)
+            sim.model_error = sim_mekls[0]
+        sim.model_time_step = sim_model_error_time_step
         lvl_ensemble.append( sim )
 
     ML_ensemble.append(lvl_ensemble)
 
     # diff-levels
     for l_idx in range(1,len(ML_Nes)):
+        print(l_idx)
         lvl_ensemble0 = []
         lvl_ensemble1 = []
         
-        data_args0 = make_init_fields(model_error_args_list[l_idx])
-        data_args1 = make_init_fields(model_error_args_list[l_idx-1])
+        data_args0 = make_data_args(args_list[l_idx])
+        data_args1 = make_data_args(args_list[l_idx-1])
         
         for e in range(ML_Nes[l_idx]):
-            sim0 = make_sim(model_error_args_list[l_idx], sample_args, init_fields=data_args0)
-            mekls[l_idx].perturbSim(sim0)
+            sim0 = make_sim(args_list[l_idx], sample_args, init_fields=data_args0)
+            sim1 = make_sim(args_list[l_idx-1], sample_args, init_fields=data_args1)
+            
+            if init_model_error_basis_args:
+                init_mekls[l_idx].perturbSim(sim0)
+                init_mekls[l_idx-1].perturbSimSimilarAs(sim1, modelError=init_mekls[l_idx])
 
-            sim1 = make_sim(model_error_args_list[l_idx-1], sample_args, init_fields=data_args1)
-            mekls[l_idx-1].perturbSimSimilarAs(sim1, modelError=mekls[l_idx])
+            if sim_model_error_basis_args:
+                sim0.model_error = sim_mekls[l_idx]
+                sim1.model_error = sim_mekls[l_idx-1]
+
+            sim0.model_time_step = sim_model_error_time_step
+            sim1.model_time_step = sim_model_error_time_step
 
             lvl_ensemble0.append(sim0)
             lvl_ensemble1.append(sim1)
--- utils/BasinInit.py
+++ utils/BasinInit.py
@@ -29,6 +29,47 @@ def make_init_fields(args):
     return {"eta": eta, "hu": hu, "hv": hv, "Hi": Hi}
 
 
+def make_init_steady_state(args, a=1, b = 2.5e-9, sample_args={"g":9.81, "f":0.0012}):
+    dataShape = (args["ny"] + 4, args["nx"] + 4)
+    dataShapeH = (args["ny"] + 5, args["nx"] + 5)
+
+    eta0 = np.zeros(dataShape, dtype=np.float32)
+    hu0  = np.zeros(dataShape, dtype=np.float32)
+    hv0  = np.zeros(dataShape, dtype=np.float32)
+    H_const = 60
+    Hi = np.ones(dataShapeH, dtype=np.float32)*H_const
+
+    def init_eta(x, y, a, b):
+        return a*np.exp(-b*(x*x + y*y))
+
+    def deta_dx(x, y, a, b):
+        return -a*2*b*x*np.exp(-b*(x*x + y*y))
+    def deta_dy(x, y, a, b):
+        return -a*2*b*y*np.exp(-b*(x*x + y*y))
+
+    def init_hv(x, y, a, b, H, g, f):
+        return  (g*(init_eta(x,y,a,b) + H)/f)*deta_dx(x, y, a, b)
+    def init_hu(x, y, a, b, H, g, f):
+        return  -(g*(init_eta(x,y,a,b) + H)/f)*deta_dy(x, y, a, b)
+
+    nx = args["nx"]
+    ny = args["ny"]
+    dx = args["dx"]
+    dy = args["dy"]
+
+    def add_bump(x_bump, y_bump, a, b):
+        for j in range(ny):
+            for i in range(nx):
+                eta0[2+j, 2+i] += init_eta((i+0.5)*dx - x_bump, (j+0.5)*dy - y_bump, a, b) 
+                hu0[2+j, 2+i] +=  init_hu( (i+0.5)*dx - x_bump, (j+0.5)*dy - y_bump, a, b, H_const, sample_args["g"], sample_args["f"])
+                hv0[2+j, 2+i] +=  init_hv( (i+0.5)*dx - x_bump, (j+0.5)*dy - y_bump, a, b, H_const, sample_args["g"], sample_args["f"])
+
+    add_bump(nx*dx/2, ny*dy/2 + ny*dy/6, a, b)
+    add_bump(nx*dx/2, ny*dy/2 - ny*dy/6, -a, b)
+
+    return {"eta": eta0, "hu": hu0, "hv": hv0, "Hi": Hi}
+
+
 def make_sim(me_args, sample_args={"g":9.81, "f":0.0012}, init_fields = None):
     if init_fields is None:
         init_fields = make_init_fields(me_args)
@@ -51,3 +92,4 @@ def make_sim(me_args, sample_args={"g":9.81, "f":0.0012}, init_fields = None):
     }
 
     return CDKLM16.CDKLM16(**sim_args)
+
