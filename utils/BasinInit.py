ae373aa2b03eded7057fc6b31dcae97a36726847
FlorianBeiser <florian.beiser@sintef.no>
2023-05-09 14:42:25 +0200

    speeding up init function

--- utils/BasinInit.py
+++ utils/BasinInit.py
@@ -39,30 +39,26 @@ def make_init_steady_state(args, a=1, b = 2.5e-9, sample_args={"g":9.81, "f":0.0
     H_const = 60
     Hi = np.ones(dataShapeH, dtype=np.float32)*H_const
 
-    def init_eta(x, y, a, b):
-        return a*np.exp(-b*(x*x + y*y))
-
-    def deta_dx(x, y, a, b):
-        return -a*2*b*x*np.exp(-b*(x*x + y*y))
-    def deta_dy(x, y, a, b):
-        return -a*2*b*y*np.exp(-b*(x*x + y*y))
-
-    def init_hv(x, y, a, b, H, g, f):
-        return  (g*(init_eta(x,y,a,b) + H)/f)*deta_dx(x, y, a, b)
-    def init_hu(x, y, a, b, H, g, f):
-        return  -(g*(init_eta(x,y,a,b) + H)/f)*deta_dy(x, y, a, b)
-
     nx = args["nx"]
     ny = args["ny"]
     dx = args["dx"]
     dy = args["dy"]
 
+
     def add_bump(x_bump, y_bump, a, b):
-        for j in range(ny):
-            for i in range(nx):
-                eta0[2+j, 2+i] += init_eta((i+0.5)*dx - x_bump, (j+0.5)*dy - y_bump, a, b) 
-                hu0[2+j, 2+i] +=  init_hu( (i+0.5)*dx - x_bump, (j+0.5)*dy - y_bump, a, b, H_const, sample_args["g"], sample_args["f"])
-                hv0[2+j, 2+i] +=  init_hv( (i+0.5)*dx - x_bump, (j+0.5)*dy - y_bump, a, b, H_const, sample_args["g"], sample_args["f"])
+        X = np.arange(start=0.5*dx, stop=nx*dx , step=dx) - x_bump
+        Y = np.arange(start=0.5*dy, stop=ny*dy , step=dy) - y_bump
+        YY, XX = np.meshgrid(X, Y)
+
+        etaBump = a*np.exp(-b*(XX*XX + YY*YY))
+        eta0[2:-2, 2:-2] += etaBump
+
+        DetaDX = -a*2*b*XX*np.exp(-b*(XX*XX + YY*YY))
+        DetaDY = -a*2*b*YY*np.exp(-b*(XX*XX + YY*YY))
+
+        hu0[2:-2,2:-2] += -(sample_args["g"]*(etaBump + H_const)/sample_args["f"])*DetaDX
+        hv0[2:-2,2:-2] += (sample_args["g"]*(etaBump + H_const)/sample_args["f"])*DetaDY
+
 
     add_bump(nx*dx/2, ny*dy/2 + ny*dy/6, a, b)
     add_bump(nx*dx/2, ny*dy/2 - ny*dy/6, -a, b)
