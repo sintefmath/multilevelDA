3512b6d6fce4e9031a84da226120356a71dcc47c
FlorianBeiser <florian.beiser@sintef.no>
2023-05-22 08:45:59 +0200

    minor bug fix: checking for none

--- utils/BasinEnsembleInit.py
+++ utils/BasinEnsembleInit.py
@@ -27,12 +27,12 @@ def initMLensemble(ML_Nes, args_list, make_data_args, sample_args,
             data_args_list.append( make_data_args(args_list[l_idx]) )
 
     # Model errors
-    if init_model_error_basis_args: 
+    if init_model_error_basis_args is not None: 
         init_mekls = []
         for l_idx in range(len(args_list)): 
             init_mekls.append( ModelErrorKL.ModelErrorKL(**args_list[l_idx], **init_model_error_basis_args) )
 
-    if sim_model_error_basis_args: 
+    if sim_model_error_basis_args is not None: 
         sim_mekls = []
         for l_idx in range(len(args_list)): 
             sim_mekls.append( ModelErrorKL.ModelErrorKL(**args_list[l_idx], **sim_model_error_basis_args) )
@@ -47,9 +47,9 @@ def initMLensemble(ML_Nes, args_list, make_data_args, sample_args,
     for i in range(ML_Nes[0]):
         if i % 100 == 0: print(i)
         sim = make_sim(args_list[0], sample_args, init_fields=data_args_list[0])
-        if init_model_error_basis_args:
+        if init_model_error_basis_args is not None:
             init_mekls[0].perturbSim(sim)
-        if sim_model_error_basis_args:
+        if sim_model_error_basis_args is not None:
             # sim.setKLModelError(**sim_model_error_basis_args)
             sim.model_error = sim_mekls[0]
         sim.model_time_step = sim_model_error_time_step
@@ -67,11 +67,11 @@ def initMLensemble(ML_Nes, args_list, make_data_args, sample_args,
             sim0 = make_sim(args_list[l_idx], sample_args, init_fields=data_args_list[l_idx])
             sim1 = make_sim(args_list[l_idx-1], sample_args, init_fields=data_args_list[l_idx-1])
             
-            if init_model_error_basis_args:
+            if init_model_error_basis_args is not None:
                 init_mekls[l_idx].perturbSim(sim0)
                 init_mekls[l_idx-1].perturbSimSimilarAs(sim1, modelError=init_mekls[l_idx])
 
-            if sim_model_error_basis_args:
+            if sim_model_error_basis_args is not None:
                 sim0.model_error = sim_mekls[l_idx]
                 sim1.model_error = sim_mekls[l_idx-1]
 
