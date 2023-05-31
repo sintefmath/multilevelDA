26d801230a2431e6870482962ca398d63a25d03f
FlorianBeiser <florian.beiser@sintef.no>
2023-05-31 15:38:51 +0200

    reducing duplication

--- scripts/BasinVarianceLevelsDAfineK.py
+++ scripts/BasinVarianceLevelsDAfineK.py
@@ -84,8 +84,8 @@ Ts = [0, 15*60, 3600, 6*3600, 12*3600]
 # Flags for model error
 import argparse
 parser = argparse.ArgumentParser(description='Generate an ensemble.')
-parser.add_argument('--Nvar', type=int, default=5)
-parser.add_argument('--Ne', type=int, default=150)
+parser.add_argument('--Nvar', type=int, default=2)
+parser.add_argument('--Ne', type=int, default=100)
 
 pargs = parser.parse_args()
 
@@ -211,7 +211,7 @@ truth.model_time_step = sim_model_error_timestep
 SL_ensembles = []
 SL_ensembles.append([])
 for l_idx in range(1,len(ls)):
-    SL_ensembles.append([[],[]])
+    SL_ensembles.append([])
 
 
 for e in range(Ne):
@@ -226,20 +226,14 @@ for e in range(Ne):
         init_mekls[l_idx].perturbSimSimilarAs(sim, modelError=init_mekls[0])
         sim.model_error = sim_mekls[l_idx]
         sim.model_time_step = sim_model_error_timestep
-        SL_ensembles[l_idx][0].append( sim )
-
-        coarse_sim = CDKLM16.CDKLM16(**sim_args_list[l_idx-1])
-        init_mekls[l_idx-1].perturbSimSimilarAs(coarse_sim, modelError=init_mekls[0])
-        coarse_sim.model_error = sim_mekls[l_idx-1]
-        coarse_sim.model_time_step = sim_model_error_timestep
-        SL_ensembles[l_idx][1].append( coarse_sim )
+        SL_ensembles[l_idx].append( sim )
 
 # %%
 # Weights
 localisation_weights_list = len(obs_xs)*[None]
 if localisation:
     for h, [obs_x, obs_y] in enumerate(zip(obs_xs, obs_ys)):
-        localisation_weights_list[h] = GCweights(SL_ensembles[-1][0], obs_x, obs_y, r) 
+        localisation_weights_list[h] = GCweights(SL_ensembles[-1], obs_x, obs_y, r) 
 
 # %%
 ##########################
@@ -258,15 +252,10 @@ for n in range(N_var):
         init_mekls[0].perturbSim(SL_ensembles[0][e])
 
         for l_idx in range(1, len(ls)):    
-            SL_ensembles[l_idx][0][e].upload(data_args_list[l_idx]["eta"], data_args_list[l_idx]["hu"], data_args_list[l_idx]["hv"])
-            SL_ensembles[l_idx][0][e].t = 0.0
-            init_mekls[l_idx].perturbSimSimilarAs(SL_ensembles[l_idx][0][e], modelError=init_mekls[0])
+            SL_ensembles[l_idx][e].upload(data_args_list[l_idx]["eta"], data_args_list[l_idx]["hu"], data_args_list[l_idx]["hv"])
+            SL_ensembles[l_idx][e].t = 0.0
+            init_mekls[l_idx].perturbSimSimilarAs(SL_ensembles[l_idx][e], modelError=init_mekls[0])
             
-            SL_ensembles[l_idx][1][e].upload(data_args_list[l_idx-1]["eta"], data_args_list[l_idx-1]["hu"], data_args_list[l_idx-1]["hv"])
-            SL_ensembles[l_idx][1][e].t = 0.0
-            init_mekls[l_idx-1].perturbSimSimilarAs(SL_ensembles[l_idx][1][e], modelError=init_mekls[0])
-            
-
     ##########################
     # loop over time
     t_now = 0.0
@@ -284,15 +273,11 @@ for n in range(N_var):
                     SL_ensembles[0][e].step(sim_model_error_timestep)
                     sim_mekls[0].perturbSim(SL_ensembles[0][e])
                     for l_idx in range(1, len(ls)):
-                        SL_ensembles[l_idx][0][e].step(sim_model_error_timestep)
-                        sim_mekls[l_idx].perturbSimSimilarAs(SL_ensembles[l_idx][0][e], modelError=sim_mekls[0])
+                        SL_ensembles[l_idx][e].step(sim_model_error_timestep)
+                        sim_mekls[l_idx].perturbSimSimilarAs(SL_ensembles[l_idx][e], modelError=sim_mekls[0])
 
-                        SL_ensembles[l_idx][1][e].step(sim_model_error_timestep)
-                        sim_mekls[l_idx-1].perturbSimSimilarAs(SL_ensembles[l_idx][1][e], modelError=sim_mekls[0])
-                
                 t_now = t_now + sim_model_error_timestep
-            
-            print(t_now, SL_ensembles[0][0].t)
+                print(datetime.datetime.now().strftime("%Y-%m-%dT%H_%M_%S"), ": ", t_now)
 
             # Update step
             if DAstep < numDAsteps-1 and truth.t <= T_da:
@@ -301,63 +286,32 @@ for n in range(N_var):
                     Hx, Hy = SLobsCoord2obsIdx(truth, obs_x, obs_y)
                     obs = [true_eta[Hy,Hx], true_hu[Hy,Hx], true_hv[Hy,Hx]] + np.random.normal(0,R)
 
-                    SL_K, SL_perts = SLEnKF(SL_ensembles[-1][0], obs, obs_x, obs_y, R=R, obs_var=obs_var, 
+                    SL_K, SL_perts = SLEnKF(SL_ensembles[-1], obs, obs_x, obs_y, R=R, obs_var=obs_var, 
                             relax_factor=relax_factor, localisation_weights=localisation_weights_list[h],
                             return_perts=True)
 
-                    # Update coarse ensemble
-                    lvlHx, lvlHy = SLobsCoord2obsIdx(SL_ensembles[-1][1], obs_x, obs_y)
-                    coarse_SL_K = block_reduce(SL_K, block_size=(1,2,2,1), func=np.mean)
-
-                    coarse_SL_state = SLdownload(SL_ensembles[-1][1])
-                    coarse_SL_state = coarse_SL_state + (coarse_SL_K @ (obs[obs_var,np.newaxis] - coarse_SL_state[obs_var,lvlHy,lvlHx] - SL_perts.T))
-                    SLupload(SL_ensembles[-1][1], coarse_SL_state)
-
-
-                    # Update 0-level ensemble
-                    lvlHx, lvlHy = SLobsCoord2obsIdx(SL_ensembles[0], obs_x, obs_y)
-                    coarse_SL_K = block_reduce(SL_K, block_size=(1,2**(len(ls)-1),2**(len(ls)-1),1), func=np.mean)
-
-                    coarse_SL_state = SLdownload(SL_ensembles[0])
-                    coarse_SL_state = coarse_SL_state + (coarse_SL_K @ (obs[obs_var,np.newaxis] - coarse_SL_state[obs_var,lvlHy,lvlHx] - SL_perts.T))
-                    SLupload(SL_ensembles[0], coarse_SL_state)
-
-                    for l_idx in range(1, len(ls)-1):
-                        # Update l+ ensemble
-                        lvlHx, lvlHy = SLobsCoord2obsIdx(SL_ensembles[l_idx][0], obs_x, obs_y)
+                    for l_idx in range(len(ls)-1):
+                        # Update l ensemble
+                        lvlHx, lvlHy = SLobsCoord2obsIdx(SL_ensembles[l_idx], obs_x, obs_y)
                         coarse_SL_K = block_reduce(SL_K, block_size=(1,2**(len(ls)-1-l_idx),2**(len(ls)-1-l_idx),1), func=np.mean)
 
-                        coarse_SL_state = SLdownload(SL_ensembles[l_idx][0])
+                        coarse_SL_state = SLdownload(SL_ensembles[l_idx])
                         coarse_SL_state = coarse_SL_state + (coarse_SL_K @ (obs[obs_var,np.newaxis] - coarse_SL_state[obs_var,lvlHy,lvlHx] - SL_perts.T))
-                        SLupload(SL_ensembles[l_idx][0], coarse_SL_state)
-
-                        # Update l- ensemble
-                        lvlHx, lvlHy = SLobsCoord2obsIdx(SL_ensembles[l_idx][1], obs_x, obs_y)
-                        coarse_SL_K = block_reduce(SL_K, block_size=(1,2**(len(ls)-l_idx),2**(len(ls)-l_idx),1), func=np.mean)
-
-                        coarse_SL_state = SLdownload(SL_ensembles[l_idx][1])
-                        coarse_SL_state = coarse_SL_state + (coarse_SL_K @ (obs[obs_var,np.newaxis] - coarse_SL_state[obs_var,lvlHy,lvlHx] - SL_perts.T))
-                        SLupload(SL_ensembles[l_idx][1], coarse_SL_state)
-
+                        SLupload(SL_ensembles[l_idx], coarse_SL_state)
+  
 
         print("Saving estimator variance at t=" + str(truth.t))
-        lvlvarsTs[0][t_idx][n] = np.mean(np.var(g_functional(SL_ensembles[0]), axis=-1), axis=(1,2))
-
-        center_N = int(args_list[0]["nx"]/4)
-        center_x = int(args_list[0]["nx"]/2)
-        center_y = int(args_list[0]["ny"]/2)
-        center_lvlvarsTs[l_idx][t_idx][n] = np.mean(np.var(g_functional(SL_ensembles[0])[:, center_y-center_N:center_y+center_N, center_x-center_N:center_x+center_N,:], axis=-1), axis=(1,2))
-            
         for l_idx in range(1,len(ls)):
-            lvlvarsTs[l_idx][t_idx][n] = np.mean(np.var(g_functional(SL_ensembles[l_idx][0]), axis=-1), axis=(1,2))
+            lvlvarsTs[l_idx][t_idx][n] = np.mean(np.var(g_functional(SL_ensembles[l_idx]), axis=-1), axis=(1,2))
 
             center_N = int(args_list[l_idx]["nx"]/4)
             center_x = int(args_list[l_idx]["nx"]/2)
             center_y = int(args_list[l_idx]["ny"]/2)
-            center_lvlvarsTs[l_idx][t_idx][n] = np.mean(np.var(g_functional(SL_ensembles[l_idx][0])[:, center_y-center_N:center_y+center_N, center_x-center_N:center_x+center_N,:], axis=-1), axis=(1,2))
-            
-            difflvlvarsTs[l_idx-1][t_idx][n] = np.mean(np.var(g_functional(SL_ensembles[l_idx][0]) - g_functional(SL_ensembles[l_idx][1]).repeat(2,1).repeat(2,2), axis=-1), axis=(1,2))
-            center_difflvlvarsTs[l_idx-1][t_idx][n] = np.mean(np.var((g_functional(SL_ensembles[l_idx][0]) - g_functional(SL_ensembles[l_idx][1]).repeat(2,1).repeat(2,2))[:, center_y-center_N:center_y+center_N, center_x-center_N:center_x+center_N,:], axis=-1), axis=(1,2))
+            center_lvlvarsTs[l_idx][t_idx][n] = np.mean(np.var(g_functional(SL_ensembles[l_idx])[:, center_y-center_N:center_y+center_N, center_x-center_N:center_x+center_N,:], axis=-1), axis=(1,2))
+                
+            if l_idx > 0:
+                difflvlvarsTs[l_idx-1][t_idx][n] = np.mean(np.var(g_functional(SL_ensembles[l_idx]) - g_functional(SL_ensembles[l_idx-1]).repeat(2,1).repeat(2,2), axis=-1), axis=(1,2))
+                center_difflvlvarsTs[l_idx-1][t_idx][n] = np.mean(np.var((g_functional(SL_ensembles[l_idx]) - g_functional(SL_ensembles[l_idx-1]).repeat(2,1).repeat(2,2))[:, center_y-center_N:center_y+center_N, center_x-center_N:center_x+center_N,:], axis=-1), axis=(1,2))
 
 
         # Remaining Update step
@@ -367,43 +321,18 @@ for n in range(N_var):
                 Hx, Hy = SLobsCoord2obsIdx(truth, obs_x, obs_y)
                 obs = [true_eta[Hy,Hx], true_hu[Hy,Hx], true_hv[Hy,Hx]] + np.random.normal(0,R)
 
-                SL_K, SL_perts = SLEnKF(SL_ensembles[-1][0], obs, obs_x, obs_y, R=R, obs_var=obs_var, 
+                SL_K, SL_perts = SLEnKF(SL_ensembles[-1], obs, obs_x, obs_y, R=R, obs_var=obs_var, 
                         relax_factor=relax_factor, localisation_weights=localisation_weights_list[h],
                         return_perts=True)
 
-                # Update coarse ensemble
-                lvlHx, lvlHy = SLobsCoord2obsIdx(SL_ensembles[-1][1], obs_x, obs_y)
-                coarse_SL_K = block_reduce(SL_K, block_size=(1,2,2,1), func=np.mean)
-
-                coarse_SL_state = SLdownload(SL_ensembles[-1][1])
-                coarse_SL_state = coarse_SL_state + (coarse_SL_K @ (obs[obs_var,np.newaxis] - coarse_SL_state[obs_var,lvlHy,lvlHx] - SL_perts.T))
-                SLupload(SL_ensembles[-1][1], coarse_SL_state)
-
-
-                # Update 0-level ensemble
-                lvlHx, lvlHy = SLobsCoord2obsIdx(SL_ensembles[0], obs_x, obs_y)
-                coarse_SL_K = block_reduce(SL_K, block_size=(1,2**(len(ls)-1),2**(len(ls)-1),1), func=np.mean)
-
-                coarse_SL_state = SLdownload(SL_ensembles[0])
-                coarse_SL_state = coarse_SL_state + (coarse_SL_K @ (obs[obs_var,np.newaxis] - coarse_SL_state[obs_var,lvlHy,lvlHx] - SL_perts.T))
-                SLupload(SL_ensembles[0], coarse_SL_state)
-
-                for l_idx in range(1, len(ls)-1):
-                    # Update l+ ensemble
-                    lvlHx, lvlHy = SLobsCoord2obsIdx(SL_ensembles[l_idx][0], obs_x, obs_y)
+                for l_idx in range(len(ls)-1):
+                    # Update l ensemble
+                    lvlHx, lvlHy = SLobsCoord2obsIdx(SL_ensembles[l_idx], obs_x, obs_y)
                     coarse_SL_K = block_reduce(SL_K, block_size=(1,2**(len(ls)-1-l_idx),2**(len(ls)-1-l_idx),1), func=np.mean)
 
-                    coarse_SL_state = SLdownload(SL_ensembles[l_idx][0])
+                    coarse_SL_state = SLdownload(SL_ensembles[l_idx])
                     coarse_SL_state = coarse_SL_state + (coarse_SL_K @ (obs[obs_var,np.newaxis] - coarse_SL_state[obs_var,lvlHy,lvlHx] - SL_perts.T))
-                    SLupload(SL_ensembles[l_idx][0], coarse_SL_state)
-
-                    # Update l- ensemble
-                    lvlHx, lvlHy = SLobsCoord2obsIdx(SL_ensembles[l_idx][1], obs_x, obs_y)
-                    coarse_SL_K = block_reduce(SL_K, block_size=(1,2**(len(ls)-l_idx),2**(len(ls)-l_idx),1), func=np.mean)
-
-                    coarse_SL_state = SLdownload(SL_ensembles[l_idx][1])
-                    coarse_SL_state = coarse_SL_state + (coarse_SL_K @ (obs[obs_var,np.newaxis] - coarse_SL_state[obs_var,lvlHy,lvlHx] - SL_perts.T))
-                    SLupload(SL_ensembles[l_idx][1], coarse_SL_state)
+                    SLupload(SL_ensembles[l_idx], coarse_SL_state)
 
 
 # %% 
