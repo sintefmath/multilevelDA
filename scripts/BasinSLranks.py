bb5e7915acafc3edcc74746157635d4575138f3f
FlorianBeiser <florian.beiser@sintef.no>
2023-06-15 13:20:27 +0200

    SL ranks run

--- scripts/BasinSLranks.py
+++ scripts/BasinSLranks.py
@@ -16,6 +16,10 @@ from matplotlib import pyplot as plt
 
 import pycuda.driver as cuda
 
+# import time
+# print("Gonna sleep now!")
+# time.sleep(3*3600) # Sleep for 3 seconds
+
 # %% 
 import signal
 
@@ -57,77 +61,28 @@ gpu_stream = cuda.Stream()
 # ## Setting-up case with different resolutions
 
 # %%
-L = 9
+L = 10
 
 # %% 
-sample_args = {
-    "g": 9.81,
-    "f": 0.0012,
-    }
-
-
-# %% 
-init_model_error_basis_args = {
-    "basis_x_start": 1, 
-    "basis_x_end": 6,
-    "basis_y_start": 2,
-    "basis_y_end": 7,
-
-    "kl_decay": 1.25,
-    "kl_scaling": 0.18,
-}
-
-# %% 
-sim_model_error_basis_args = {
-    "basis_x_start": 2, 
-    "basis_x_end": 7,
-    "basis_y_start": 3,
-    "basis_y_end": 8,
-
-    "kl_decay": 1.25,
-    "kl_scaling": 0.004,
-}
+from utils.BasinParameters import *
 
 # %% 
 # Flags for model error
 import argparse
 parser = argparse.ArgumentParser(description='Generate an ensemble.')
 parser.add_argument('--N', type=int, default=100)
-parser.add_argument('--init_error', type=int, default=1,choices=[0,1])
-parser.add_argument('--sim_error', type=int, default=1,choices=[0,1])
-parser.add_argument('--sim_error_timestep', type=float, default=60) 
-
 
 pargs = parser.parse_args()
 
-init_model_error = bool(pargs.init_error)
-sim_model_error = bool(pargs.sim_error)
-sim_model_error_timestep = pargs.sim_error_timestep
-
-# init_model_error = True
-# sim_model_error = True
-# sim_model_error_timestep = 60.0
-
 N_ranks = pargs.N
 
-# %% [markdown] 
+# %%
 # ## Ensemble
-
-# %% 
 Ne = 100
 
 # %% 
-# Truth observation
-Hx, Hy = 250, 500
-R = [0.1, 1.0, 1.0]
-
-# %% 
 # Assimilation
-localisation = False
-r = 2.5e4
-relax_factor = 0.1
-
-da_timestep = 900
+localisation = True
 
 # %% 
 # Simulation
@@ -147,32 +102,24 @@ log.write("Init State\n")
 log.write("Lake-at-rest\n\n")
 
 log.write("Init Perturbation\n")
-if init_model_error:
-    log.write("KL bases x start: " + str(init_model_error_basis_args["basis_x_start"]) + "\n")
-    log.write("KL bases x end: " + str(init_model_error_basis_args["basis_x_end"]) + "\n")
-    log.write("KL bases y start: " + str(init_model_error_basis_args["basis_y_start"]) + "\n")
-    log.write("KL bases y end: " + str(init_model_error_basis_args["basis_y_end"]) + "\n")
-    log.write("KL decay: " + str(init_model_error_basis_args["kl_decay"]) +"\n")
-    log.write("KL scaling: " + str(init_model_error_basis_args["kl_scaling"]) + "\n\n")
-else: 
-    init_model_error_basis_args = None
-    log.write("False\n\n")
+log.write("KL bases x start: " + str(init_model_error_basis_args["basis_x_start"]) + "\n")
+log.write("KL bases x end: " + str(init_model_error_basis_args["basis_x_end"]) + "\n")
+log.write("KL bases y start: " + str(init_model_error_basis_args["basis_y_start"]) + "\n")
+log.write("KL bases y end: " + str(init_model_error_basis_args["basis_y_end"]) + "\n")
+log.write("KL decay: " + str(init_model_error_basis_args["kl_decay"]) +"\n")
+log.write("KL scaling: " + str(init_model_error_basis_args["kl_scaling"]) + "\n\n")
 
 log.write("Temporal Perturbation\n")
-if sim_model_error:
-    log.write("Model error timestep: " + str(sim_model_error_timestep) +"\n")
-    log.write("KL bases x start: " + str(sim_model_error_basis_args["basis_x_start"]) + "\n")
-    log.write("KL bases x end: " + str(sim_model_error_basis_args["basis_x_end"]) + "\n")
-    log.write("KL bases y start: " + str(sim_model_error_basis_args["basis_y_start"]) + "\n")
-    log.write("KL bases y end: " + str(sim_model_error_basis_args["basis_y_end"]) + "\n")
-    log.write("KL decay: " + str(sim_model_error_basis_args["kl_decay"]) +"\n")
-    log.write("KL scaling: " + str(sim_model_error_basis_args["kl_scaling"]) + "\n\n")
-else:
-    sim_model_error_basis_args = None
-    log.write("False\n\n")
+log.write("Model error timestep: " + str(sim_model_error_timestep) +"\n")
+log.write("KL bases x start: " + str(sim_model_error_basis_args["basis_x_start"]) + "\n")
+log.write("KL bases x end: " + str(sim_model_error_basis_args["basis_x_end"]) + "\n")
+log.write("KL bases y start: " + str(sim_model_error_basis_args["basis_y_start"]) + "\n")
+log.write("KL bases y end: " + str(sim_model_error_basis_args["basis_y_end"]) + "\n")
+log.write("KL decay: " + str(sim_model_error_basis_args["kl_decay"]) +"\n")
+log.write("KL scaling: " + str(sim_model_error_basis_args["kl_scaling"]) + "\n\n")
 
 log.write("Truth\n")
-log.write("Hx, Hy: " + str(Hx) + ", " + str(Hy) + "\n")
+log.write("obs_x, obs_y: " + " / ".join([str(obs_x) + ", " + str(obs_y)   for obs_x, obs_y in zip(obs_xs,obs_ys)]) + "\n")
 log.write("R = " + ", ".join([str(Rii) for Rii in R])+"\n\n")
 
 log.write("Assimilation\n")
@@ -199,28 +146,23 @@ args = {
     "boundary_conditions": Common.BoundaryConditions(2,2,2,2)
     }
 
-data_args = make_init_steady_state(args)
+data_args = make_init_steady_state(args, a=steady_state_bump_a, bump_fractal_dist=steady_state_bump_fractal_dist)
 
 # %% 
 # Truth
 truth = make_sim(args, sample_args=sample_args, init_fields=data_args)
-if init_model_error:
-    init_mekl = ModelErrorKL.ModelErrorKL(**args, **init_model_error_basis_args)
-    init_mekl.perturbSim(truth)
-if sim_model_error:
-    truth.setKLModelError(**sim_model_error_basis_args)
-    truth.model_time_step = sim_model_error_timestep
+truth_init_mekl = ModelErrorKL.ModelErrorKL(**args, **init_model_error_basis_args)
+truth_init_mekl.perturbSim(truth)
+truth.setKLModelError(**sim_model_error_basis_args)
+truth.model_time_step = sim_model_error_timestep
 
 # %% 
 # Initialise ensemble
 # Since we aim to re-use all objects, we do NOT use the `BasinEnsembleInit.py`
 
 # Model errors
-if init_model_error: 
-    init_mekl = ModelErrorKL.ModelErrorKL(**args, **init_model_error_basis_args) 
-
-if sim_model_error: 
-    sim_mekl = ModelErrorKL.ModelErrorKL(**args, **sim_model_error_basis_args)
+init_mekl = ModelErrorKL.ModelErrorKL(**args, **init_model_error_basis_args) 
+sim_mekl = ModelErrorKL.ModelErrorKL(**args, **sim_model_error_basis_args)
 
 
 # Ensemble
@@ -231,10 +173,12 @@ SL_ensemble = initSLensemble(Ne, args, data_args, sample_args,
 
 localisation_weights = None
 if localisation:
-    localisation_weights = GCweights(SL_ensemble, Hx, Hy, r) 
+    localisation_weights_list = []
+    for obs_x, obs_y in zip(obs_xs, obs_ys):
+        localisation_weights_list.append( GCweights(SL_ensemble, obs_x, obs_y, r) ) 
 
 # %%
-freq = 50
+freq = int(np.floor(grid_args["nx"]/20))
 rankHxs = np.arange(0, grid_args["nx"], freq)
 rankHys = np.arange(0, grid_args["ny"], 2*freq)
 
@@ -249,22 +193,20 @@ while n < N_ranks:
     print("\n\nExperiment: ", n)
 
     try:
-        signal.alarm(5*60)
+        signal.alarm(60*60)
 
         # New Truth
         print("Make a new truth")
         truth.upload(data_args["eta"], data_args["hu"], data_args["hv"])
         truth.t = 0.0
-        if init_model_error:
-            init_mekl.perturbSim(truth)
+        truth_init_mekl.perturbSim(truth)
 
         # New Ensemble
         # 0-level
         for e in range(Ne):
             SL_ensemble[e].upload(data_args["eta"], data_args["hu"], data_args["hv"])
             SL_ensemble[e].t = 0.0
-            if init_model_error:
-                init_mekl.perturbSim(SL_ensemble[e])
+            init_mekl.perturbSim(SL_ensemble[e])
 
         print("Lets start to move")
         t_now = 0.0
@@ -273,30 +215,34 @@ while n < N_ranks:
             numDAsteps = int((T-t_now)/da_timestep)  
 
             for step in range(numDAsteps):
+                # Forward step
                 truth.dataAssimilationStep(t_now+da_timestep)
                 SLstepToObservation(SL_ensemble, t_now+da_timestep)
                 t_now += da_timestep
 
+                # DA step
                 if step < numDAsteps-1:
-                    print("non-recorded DA")
+                    print("non-recorded DA at " + str(truth.t))
                     true_eta, true_hu, true_hv = truth.download(interior_domain_only=True)
-                    obs = [true_eta[Hy,Hx], true_hu[Hy,Hx], true_hv[Hy,Hx]] + np.random.normal(0,R)
+                    for o, [obs_x, obs_y] in enumerate(zip(obs_xs, obs_ys)):
+                        Hx, Hy = SLobsCoord2obsIdx(SL_ensemble, obs_x, obs_y)
+                        obs = [true_eta[Hy,Hx], true_hu[Hy,Hx], true_hv[Hy,Hx]] + np.random.normal(0,R)
 
-                    SLEnKF(SL_ensemble, obs, Hx, Hy, R=R, obs_var=slice(1,3), 
-                            relax_factor=relax_factor, localisation_weights=localisation_weights)
+                        SLEnKF(SL_ensemble, obs, obs_x, obs_y, R=R, obs_var=slice(1,3), 
+                            relax_factor=relax_factor, localisation_weights=localisation_weights_list[o])
 
-            print("recorded DA")
+            print("recorded DA at " + str(truth.t))
             SL_prior_ranksTs[t_idx][n*len(rankHxs):(n+1)*len(rankHxs)] = SLrank(SL_ensemble, truth, [z for z in zip(rankHxs, rankHys)], R)
 
             true_eta, true_hu, true_hv = truth.download(interior_domain_only=True)
-            obs = [true_eta[Hy,Hx], true_hu[Hy,Hx], true_hv[Hy,Hx]] + np.random.normal(0,R)
+            for o, [obs_x, obs_y] in enumerate(zip(obs_xs, obs_ys)):
+                Hx, Hy = SLobsCoord2obsIdx(SL_ensemble, obs_x, obs_y)
+                obs = [true_eta[Hy,Hx], true_hu[Hy,Hx], true_hv[Hy,Hx]] + np.random.normal(0,R)
 
-            SLEnKF(SL_ensemble, obs, Hx, Hy, R=R, obs_var=slice(1,3), 
-                    relax_factor=relax_factor, localisation_weights=localisation_weights)
+                SLEnKF(SL_ensemble, obs, obs_x, obs_y, R=R, obs_var=slice(1,3), 
+                    relax_factor=relax_factor, localisation_weights=localisation_weights_list[o])
 
             SL_posterior_ranksTs[t_idx][n*len(rankHxs):(n+1)*len(rankHxs)] = SLrank(SL_ensemble, truth, [z for z in zip(rankHxs, rankHys)], R)
-
-            print(T)
 
         for t_idx, T in enumerate(Ts):
             np.save(output_path+"/dump/SLpriorRanks_"+str(T)+"_dump_"+str(n)+".npy", SL_prior_ranksTs[t_idx][n*len(rankHxs):(n+1)*len(rankHxs)])
