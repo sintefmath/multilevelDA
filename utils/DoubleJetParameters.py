5c600ff0122297e3dae77df487d8d43e86d24008
FlorianBeiser <florian.beiser@sintef.no>
2023-06-29 09:55:00 +0200

    testing new parameters

--- utils/DoubleJetParameters.py
+++ utils/DoubleJetParameters.py
@@ -13,15 +13,15 @@ sim_model_error_basis_args = {
                             "basis_y_end": 7,
 
                             "kl_decay": 1.25,
-                            "kl_scaling": 0.025,
+                            "kl_scaling": 0.01,
                             }
 
 sim_model_error_timestep=60.0
 
 # Parameters for time
-T_spinup = 6*3600
-T_da = 6*3600
-T_forecast = 6*3600
+T_spinup = 12*3600
+T_da = 48*3600
+T_forecast = 24*3600
 
 # Parameters for truth obervation
 xdim = 1332000.0
@@ -32,12 +32,12 @@ ys = np.linspace(0, ydim, 5, endpoint=False)
 
 [obs_xs, obs_ys] = np.array(np.meshgrid(xs, ys)).T.reshape(-1, 2).T
 
-R = [0.05, 1.0, 1.0]
+R = [0.05, 5.0, 5.0]
 
 # Parameters for assimilation
 obs_var = slice(1,3)
 r = 5e4
-relax_factor = 0.025
+relax_factor = 0.1
 min_location_level = 0
 
 da_timestep = 900
