7306cca06e0c9786f6e8605e5c0fab8ed3d914dc
FlorianBeiser <florian.beiser@sintef.no>
2023-08-18 11:50:47 +0200

    Convergence of the Kalman gain estimator: Scripts and notebook

---
+++ scripts/BasinKGainConv.py
@@ -0,0 +1,305 @@
+# %% [markdown]
+# # Kalman Gain convergence
+
+# %% [markdown]
+# ### Classes and modules
+
+# %%
+import os, sys
+
+#Import packages we need
+import numpy as np
+import datetime
+import copy
+
+#For plotting
+import matplotlib
+from matplotlib import pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+plt.rcParams["image.origin"] = "lower"
+
+import pycuda.driver as cuda
+
+# %%
+import datetime
+timestamp = datetime.datetime.now().strftime("%Y-%m-%dT%H_%M_%S")
+
+output_path = "KGainConvergence/Basin/"+timestamp 
+os.makedirs(output_path)
+
+# %%
+import shutil
+shutil.copy(__file__, output_path + os.sep + "script_copy.py")
+
+log = open(output_path+"/log.txt", 'w')
+log.write("Parameters for Kalman Gain Convergence\n\n")
+
+gpuocean_path = [p[:-4] for p in sys.path if (p.endswith("gpuocean/src") or p.endswith("gpuocean\\src"))][0]
+import git
+gpuocean_repo = git.Repo(gpuocean_path)
+log.write("GPUOcean code from: " + str(gpuocean_repo.head.object.hexsha) + " on branch " + str(gpuocean_repo.active_branch.name) + "\n")
+
+repo = git.Repo(search_parent_directories=True)
+log.write("Current repo >>"+str(repo.working_tree_dir.split("/")[-1])+"<< with " +str(repo.head.object.hexsha)+ "on branch " + str(repo.active_branch.name) + "\n\n")
+
+
+# %% [markdown]
+# GPU Ocean-modules:
+
+# %%
+from gpuocean.utils import Common
+from gpuocean.SWEsimulators import CDKLM16, ModelErrorKL
+from gpuocean.ensembles import MultiLevelOceanEnsemble
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), '../')))
+from utils.BasinInit import *
+from utils.BasinPlot import *
+from utils.BasinAnalysis import *
+from utils.BasinSL import *
+from utils.BasinEnsembleInit import *
+
+# %%
+gpu_ctx = Common.CUDAContext()
+gpu_stream = cuda.Stream()
+
+
+# %%
+ls = [6, 7, 8, 9, 10]
+
+# %%
+args_list = []
+
+for l in ls:
+    lvl_grid_args = initGridSpecs(l)
+    args_list.append( {
+        "nx": lvl_grid_args["nx"],
+        "ny": lvl_grid_args["ny"],
+        "dx": lvl_grid_args["dx"],
+        "dy": lvl_grid_args["dy"],
+        "gpu_ctx": gpu_ctx,
+        "gpu_stream": gpu_stream,
+        "boundary_conditions": Common.BoundaryConditions(2,2,2,2)
+        } )
+
+# %% [markdown]
+# ### Multi-Level Ensemble Sizes 
+
+# %%
+source_path = "/home/florianb/havvarsel/multilevelDA/scripts/VarianceLevelsDA/Basin/2023-06-28T11_25_15sharedOmegaLocalised"
+
+center_vars = np.load(source_path+"/center_vars_21600_L2norm.npy")[:-1]
+center_diff_vars = np.load(source_path+"/center_diff_vars_21600_L2norm.npy")[:-1]
+
+# %%
+work_path = "/home/florianb/havvarsel/multilevelDA/scripts/PracticalCost/Basin/2023-06-29T14_54_02"
+
+# %%
+def raw2costsEnsemble(filename):
+    rawCosts = np.load(filename)
+    return np.mean(np.sort(rawCosts), axis=1)
+
+# %%
+costsPure = raw2costsEnsemble(work_path+"/costsPureEnsemble.npy")[:-1]
+costsPartnered = raw2costsEnsemble(work_path+"/costsPartneredEnsemble.npy")[:-1]
+
+# %%
+analysis = Analysis(args_list[:-1], center_vars, center_diff_vars, costsPure, costsPartnered)
+
+# %%
+taus = [1.25e-1, 1e-1, 7.5e-2, 5.0e-2, 3.5e-2, 2.5e-2]
+
+Nes = []
+
+for tau in taus:
+    ML_Nes = analysis.optimal_Ne(tau=tau)
+    SL_Ne = np.ceil(analysis.work(ML_Nes)/costsPure[-1]).astype(int)
+
+    Nes.append([ML_Nes, SL_Ne])
+
+
+# %%
+from utils.BasinParameters import * 
+
+# Book keeping
+log.write("levels = " + ", ".join([str(l) for l in ls])+"\n\n")
+
+log.write("ML_Nes = " + ", ".join([str(Ne[0]) for Ne in Nes])+"\n")
+log.write("ML_Nes = " + ", ".join([str(Ne[1]) for Ne in Nes])+"\n\n")
+
+log.write("nx = " + str(args_list[-1]["nx"]) + ", ny = " + str(args_list[-1]["ny"])+"\n")
+log.write("dx = " + str(args_list[-1]["dx"]) + ", dy = " + str(args_list[-1]["dy"])+"\n")
+log.write("T (DA) = " + str(T_da) +"\n")
+log.write("T (forecast) = " + str(T_forecast) +"\n\n")
+
+log.write("Init State\n")
+log.write("Double Bump\n")
+log.write("Bump size [m]: " + str(steady_state_bump_a) +"\n")
+log.write("Bump dist [fractal]: " + str(steady_state_bump_fractal_dist) + "\n\n")
+
+log.write("Init Perturbation\n")
+log.write("KL bases x start: " + str(init_model_error_basis_args["basis_x_start"]) + "\n")
+log.write("KL bases x end: " + str(init_model_error_basis_args["basis_x_end"]) + "\n")
+log.write("KL bases y start: " + str(init_model_error_basis_args["basis_y_start"]) + "\n")
+log.write("KL bases y end: " + str(init_model_error_basis_args["basis_y_end"]) + "\n")
+log.write("KL decay: " + str(init_model_error_basis_args["kl_decay"]) +"\n")
+log.write("KL scaling: " + str(init_model_error_basis_args["kl_scaling"]) + "\n\n")
+
+log.write("Temporal Perturbation\n")
+log.write("Model error timestep: " + str(sim_model_error_timestep) +"\n")
+log.write("KL bases x start: " + str(sim_model_error_basis_args["basis_x_start"]) + "\n")
+log.write("KL bases x end: " + str(sim_model_error_basis_args["basis_x_end"]) + "\n")
+log.write("KL bases y start: " + str(sim_model_error_basis_args["basis_y_start"]) + "\n")
+log.write("KL bases y end: " + str(sim_model_error_basis_args["basis_y_end"]) + "\n")
+log.write("KL decay: " + str(sim_model_error_basis_args["kl_decay"]) +"\n")
+log.write("KL scaling: " + str(sim_model_error_basis_args["kl_scaling"]) + "\n\n")
+
+log.write("Statistics\n")
+N_exp = 100
+log.write("N_exp = " + str(N_exp) + "\n")
+log.write("Metric: Variance in the Kalman Gain estimator\n")
+
+
+# %%
+from utils import VarianceStatistics as VS
+
+var_SL = len(taus)*[VS.WelfordsVariance3((args_list[-2]["ny"], args_list[-2]["nx"]))]
+var_ML = len(taus)*[VS.WelfordsVariance3((args_list[-2]["ny"], args_list[-2]["nx"]))]
+
+varNorm_SL = np.zeros((len(taus),3))
+varNorm_ML = np.zeros((len(taus),3))
+
+err_SL = np.zeros((len(taus),3))
+err_ML = np.zeros((len(taus),3))
+
+# %% 
+########################################
+# NEW truth
+print("Truth")
+if os.path.isdir("tmpTruth"):
+    assert len(os.listdir("tmpTruth")) == 0, "Please remove old files"
+        
+os.system("python BasinKGainConvSingle.py -m T")
+
+########################################
+# NEW reference
+print("Reference")
+os.system("python BasinKGainConvSingle.py -m R")
+
+if os.path.isdir("tmpRefGain"):
+    assert len(os.listdir("tmpRefGain")) == 1, "Please remove old files"
+    f = os.listdir("tmpRefGain")[0]
+    refK_eta, refK_hu, refK_hv = block_reduce(np.load(os.path.join("tmpRefGain", f))[:,:,:,0], block_size=(1,2,2), func=np.mean)
+
+    os.makedirs(output_path+"/GainFigs", exist_ok=True)
+    fig, axs = imshow3([refK_eta, refK_hu, refK_hv], eta_vlim=1e-2,huv_vlim=1)
+    fig.savefig(output_path+"/GainFigs/Ref")
+    plt.close("all")
+
+# %%
+###########################################
+# tau-LOOP
+for tau_idx in range(len(taus)): 
+    print("-----------------------------")
+    print("tau = ", taus[tau_idx])
+
+    ###########################################
+    # N-LOOP 
+    for n in range(N_exp):
+
+        # NEW SL Ensemble
+        print("SL", Nes[tau_idx][1])
+        os.system("python BasinKGainConvSingle.py -m SL -Ne "+str(Nes[tau_idx][1]))
+
+        if os.path.isdir("tmpSLGain"):
+            assert len(os.listdir("tmpSLGain")) == 1, "Please remove old files"
+            f = os.listdir("tmpSLGain")[0]
+            K_eta, K_hu, K_hv = np.load(os.path.join("tmpSLGain", f))[:,:,:,0]
+            var_SL[tau_idx].update(K_eta, K_hu, K_hv)
+
+            fig, axs = imshow3([K_eta,K_hu,K_hv], eta_vlim=1e-2,huv_vlim=1)
+            fig.savefig(output_path+"/GainFigs/SL"+str(tau_idx)+"_"+str(n))
+            plt.close("all")
+
+            err_SL[tau_idx] += 1/N_exp * np.array([np.linalg.norm(err_field) for err_field in [refK_eta-K_eta, refK_hu-K_hu, refK_hv-K_hv] ])
+
+            os.remove(os.path.join("tmpSLGain", f))
+        #TODO: Calculate variance in the error?!
+
+
+
+        # NEW ML Ensemble
+        print("ML", Nes[tau_idx][0])
+        os.system("python BasinKGainConvSingle.py -m ML -Ne "+" ".join(str(Ne) for Ne in Nes[tau_idx][0]))
+
+        if os.path.isdir("tmpMLGain"):
+            assert len(os.listdir("tmpMLGain")) == 1, "Please remove old files"
+            f = os.listdir("tmpMLGain")[0]
+            K_eta, K_hu, K_hv = np.load(os.path.join("tmpMLGain", f))[:,:,:,0]
+            var_ML[tau_idx].update(K_eta, K_hu, K_hv)
+
+            fig, axs = imshow3([K_eta,K_hu,K_hv], eta_vlim=1e-2,huv_vlim=1)
+            fig.savefig(output_path+"/GainFigs/ML"+str(tau_idx)+"_"+str(n))
+            plt.close("all")
+
+            err_ML[tau_idx] += 1/N_exp * np.array([np.linalg.norm(err_field) for err_field in [refK_eta-K_eta, refK_hu-K_hu, refK_hv-K_hv] ])
+
+            os.remove(os.path.join("tmpMLGain", f))
+        #TODO: Calculate variance in the error?!
+
+        # end: n-loop 
+
+    eta_varSL, hu_varSL, hv_varSL = var_SL[tau_idx].finalize()
+    fig, axs = imshow3var([eta_varSL, hu_varSL, hv_varSL], eta_vlim=1e-5,huv_vlim=1e-1)
+    fig.savefig(output_path+"/GainFigs/SLvar"+str(tau_idx))
+    plt.close("all")
+
+    varNorm_SL[tau_idx] = [np.linalg.norm(var_field) for var_field in [eta_varSL, hu_varSL, hv_varSL] ]
+    
+    
+    eta_varML, hu_varML, hv_varML = var_ML[tau_idx].finalize()
+    fig, axs = imshow3var([eta_varML, hu_varML, hv_varML], eta_vlim=1e-5,huv_vlim=1e-1)
+    fig.savefig(output_path+"/GainFigs/MLvar"+str(tau_idx))
+    plt.close("all")
+
+    varNorm_ML[tau_idx] = [np.linalg.norm(var_field) for var_field in [eta_varML, hu_varML, hv_varML] ]
+    
+    
+
+    # end: tau-loop
+
+# %% 
+# Saving results
+np.savetxt(output_path+"/var_SL.txt", varNorm_SL)
+np.savetxt(output_path+"/var_ML.txt", varNorm_ML)
+
+np.savetxt(output_path+"/errSLGain.txt", err_SL)
+np.savetxt(output_path+"/errMLGain.txt", err_ML)
+
+# %% 
+# Cleaning Up 
+if os.path.isdir("tmpTruth"):
+    for f in os.listdir("tmpTruth"):
+        os.remove(os.path.join("tmpTruth", f))
+if os.path.isdir("tmpTruth"):
+    os.rmdir("tmpTruth")
+
+if os.path.isdir("tmpRefGain"):
+    for f in os.listdir("tmpRefGain"):
+        os.remove(os.path.join("tmpRefGain", f))
+if os.path.isdir("tmpRefGain"):
+    os.rmdir("tmpRefGain")
+
+if os.path.isdir("tmpSLGain"):
+    for f in os.listdir("tmpSLGain"):
+        os.remove(os.path.join("tmpSLGain", f))
+if os.path.isdir("tmpSLGain"):
+    os.rmdir("tmpSLGain")
+
+if os.path.isdir("tmpMLGain"):
+    for f in os.listdir("tmpMLGain"):
+        os.remove(os.path.join("tmpMLGain", f))
+if os.path.isdir("tmpMLGain"):
+    os.rmdir("tmpMLGain")
+
+# %%
---
+++ scripts/BasinKGainConvSingle.py
@@ -0,0 +1,278 @@
+# %% [markdown]
+# # Error convergence
+
+# %% [markdown]
+# ### Classes and modules
+
+# %%
+import os, sys
+
+#Import packages we need
+import numpy as np
+import datetime
+
+import pycuda.driver as cuda
+
+
+# %% [markdown]
+# GPU Ocean-modules:
+
+# %%
+sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), '../')))
+from utils.BasinInit import *
+from utils.BasinPlot import *
+
+
+# %%
+from gpuocean.utils import Common
+
+gpu_ctx = Common.CUDAContext()
+gpu_stream = cuda.Stream()
+
+# %%
+ls = [6, 7, 8, 9, 10]
+
+# %%
+args_list = []
+
+for l in ls:
+    lvl_grid_args = initGridSpecs(l)
+    args_list.append( {
+        "nx": lvl_grid_args["nx"],
+        "ny": lvl_grid_args["ny"],
+        "dx": lvl_grid_args["dx"],
+        "dy": lvl_grid_args["dy"],
+        "gpu_ctx": gpu_ctx,
+        "gpu_stream": gpu_stream,
+        "boundary_conditions": Common.BoundaryConditions(2,2,2,2)
+        } )
+
+# %%
+from utils.BasinParameters import * 
+
+
+# %% 
+import argparse
+parser = argparse.ArgumentParser(description='Single run inputs')
+parser.add_argument("-m", "--mode", required=True, type=str, choices=["T", "R", "SL", "ML"])
+parser.add_argument("-Ne", "--ensembleSize", nargs="*", type=int)
+
+pargs = parser.parse_args()
+mode = pargs.mode
+Nes = pargs.ensembleSize
+
+# %% 
+print("Reducing T_da for debugging purposes!")
+T_da = 0
+
+# %%
+##############################################
+# TRUTH
+
+if mode == "T": 
+
+    def writeTruth2file(T):
+        true_state = truth.download(interior_domain_only=True)
+        os.makedirs("tmpTruth", exist_ok=True)
+        np.save("tmpTruth/truth_"+str(T)+".npy", np.array(true_state))
+
+    data_args = make_init_steady_state(args_list[-1], a=steady_state_bump_a, bump_fractal_dist=steady_state_bump_fractal_dist)
+
+
+    from gpuocean.SWEsimulators import ModelErrorKL
+    init_mekl = ModelErrorKL.ModelErrorKL(**args_list[-1], **init_model_error_basis_args)
+    sim_mekl = ModelErrorKL.ModelErrorKL(**args_list[-1], **sim_model_error_basis_args)
+
+    truth = make_sim(args_list[-1], sample_args=sample_args, init_fields=data_args)
+    init_mekl.perturbSim(truth)
+    truth.model_error = sim_mekl
+    truth.model_time_step = sim_model_error_timestep
+
+    writeTruth2file(int(truth.t))
+    while truth.t < T_da:
+        # Forward step
+        truth.dataAssimilationStep(truth.t+da_timestep)
+        # DA time
+        writeTruth2file(int(truth.t))
+
+
+
+##############################################
+# REFERENCE 
+
+elif mode == "R":
+    #############################
+    # Initialisation
+    data_args = make_init_steady_state(args_list[-1], a=steady_state_bump_a, bump_fractal_dist=steady_state_bump_fractal_dist)
+
+    from utils.BasinSL import *
+    SL_ensemble = initSLensemble(250, args_list[-1], data_args, sample_args, 
+                             init_model_error_basis_args=init_model_error_basis_args, 
+                             sim_model_error_basis_args=sim_model_error_basis_args, sim_model_error_time_step=sim_model_error_timestep)
+
+
+    ############################
+    # First Gain
+
+    # Observation
+    true_eta, true_hu, true_hv = np.load("tmpTruth/truth_"+str(int(SL_ensemble[0].t))+".npy")
+    
+    Hx, Hy = SLobsCoord2obsIdx(SL_ensemble, obs_xs[0], obs_ys[0])
+    obs = [true_eta[Hy,Hx], true_hu[Hy,Hx], true_hv[Hy,Hx]] + np.random.normal(0,R)
+
+    localisation_weights = GCweights(SL_ensemble, obs_xs[0], obs_ys[0], r) 
+    SL_K = SLEnKF(SL_ensemble, obs, obs_xs[0], obs_ys[0], R=R, obs_var=slice(1,3), 
+            relax_factor=relax_factor, localisation_weights=localisation_weights)
+
+    # localisation_weights_list = []
+    # for obs_x, obs_y in zip(obs_xs, obs_ys):
+    #     localisation_weights_list.append( GCweights(SL_ensemble, obs_x, obs_y, r) ) 
+
+    #####################
+    # Data assimilation
+
+    # while SL_ensemble[0].t < T_da:
+    #     # Forward step
+    #     SLstepToObservation(SL_ensemble, SL_ensemble[0].t + da_timestep)
+        
+    #     # Observation
+    #     true_eta, true_hu, true_hv = np.load("tmpTruth/truth_"+str(int(SL_ensemble[0].t))+".npy")
+        
+    #     for h, [obs_x, obs_y] in enumerate(zip(obs_xs, obs_ys)):
+    #         Hx, Hy = SLobsCoord2obsIdx(SL_ensemble, obs_x, obs_y)
+    #         obs = [true_eta[Hy,Hx], true_hu[Hy,Hx], true_hv[Hy,Hx]] + np.random.normal(0,R)
+
+    #         SL_K = SLEnKF(SL_ensemble, obs, obs_x, obs_y, R=R, obs_var=slice(1,3), 
+    #             relax_factor=relax_factor, localisation_weights=localisation_weights_list[h])
+
+    # Saving results
+    os.makedirs("tmpRefGain", exist_ok=True)
+    np.save("tmpRefGain/"+datetime.datetime.now().strftime("%Y-%m-%dT%H_%M_%S"), SL_K)
+
+    # Crash to let OS do the gc
+    os._exit(0)
+
+
+
+##############################################
+# SINGLE LEVEL
+
+elif mode == "SL":
+    #####################
+    # Initialisation
+    data_args = make_init_steady_state(args_list[-2], a=steady_state_bump_a, bump_fractal_dist=steady_state_bump_fractal_dist)
+
+    from utils.BasinSL import *
+    SL_ensemble = initSLensemble(Nes[0], args_list[-2], data_args, sample_args, 
+                             init_model_error_basis_args=init_model_error_basis_args, 
+                             sim_model_error_basis_args=sim_model_error_basis_args, sim_model_error_time_step=sim_model_error_timestep)
+
+    #####################
+    # First Gain
+
+    # Observation
+    true_eta, true_hu, true_hv = np.load("tmpTruth/truth_"+str(int(SL_ensemble[0].t))+".npy")
+    
+    Hx, Hy = SLobsCoord2obsIdx(SL_ensemble, obs_xs[0], obs_ys[0])
+    obs = [true_eta[Hy,Hx], true_hu[Hy,Hx], true_hv[Hy,Hx]] + np.random.normal(0,R)
+
+    localisation_weights = GCweights(SL_ensemble, obs_xs[0], obs_ys[0], r) 
+    SL_K = SLEnKF(SL_ensemble, obs, obs_xs[0], obs_ys[0], R=R, obs_var=slice(1,3), 
+            relax_factor=relax_factor, localisation_weights=localisation_weights)
+
+
+    # localisation_weights_list = []
+    # for obs_x, obs_y in zip(obs_xs, obs_ys):
+    #     localisation_weights_list.append( GCweights(SL_ensemble, obs_x, obs_y, r) ) 
+
+    #####################
+    # Data assimilation
+
+    # while SL_ensemble[0].t < T_da:
+    #     # Forward step
+    #     SLstepToObservation(SL_ensemble, SL_ensemble[0].t + da_timestep)
+        
+    #     # Observation
+    #     true_eta, true_hu, true_hv = np.load("tmpTruth/truth_"+str(int(SL_ensemble[0].t))+".npy")
+        
+    #     for h, [obs_x, obs_y] in enumerate(zip(obs_xs, obs_ys)):
+    #         Hx, Hy = SLobsCoord2obsIdx(SL_ensemble, obs_x, obs_y)
+    #         obs = [true_eta[Hy,Hx], true_hu[Hy,Hx], true_hv[Hy,Hx]] + np.random.normal(0,R)
+
+    #         SL_K = SLEnKF(SL_ensemble, obs, obs_x, obs_y, R=R, obs_var=slice(1,3), 
+    #             relax_factor=relax_factor, localisation_weights=localisation_weights_list[h])
+
+    # Saving results
+    os.makedirs("tmpSLGain", exist_ok=True)
+    np.save("tmpSLGain/"+datetime.datetime.now().strftime("%Y-%m-%dT%H_%M_%S"), SL_K)
+
+    # Crash to let OS do the gc
+    os._exit(0)
+
+
+
+##############################################
+# MULTI LEVEL
+
+elif mode == "ML":
+    
+    # Initialisation
+    data_args_list = []
+    for l_idx in range(len(args_list[:-1])):
+        data_args_list.append( make_init_steady_state(args_list[l_idx], a=steady_state_bump_a, bump_fractal_dist=steady_state_bump_fractal_dist) )
+    
+    from gpuocean.ensembles import MultiLevelOceanEnsembleCase
+    MLOceanEnsemble = MultiLevelOceanEnsembleCase.MultiLevelOceanEnsemble(Nes, args_list[:-1], data_args_list, sample_args, make_sim,
+                                init_model_error_basis_args=init_model_error_basis_args, 
+                                sim_model_error_basis_args=sim_model_error_basis_args, sim_model_error_timestep=sim_model_error_timestep)
+
+    from gpuocean.dataassimilation import MLEnKFOcean
+    MLEnKF = MLEnKFOcean.MLEnKFOcean(MLOceanEnsemble)
+
+
+    #####################
+    # First Gain
+
+    # Observation
+    true_eta, true_hu, true_hv = np.load("tmpTruth/truth_"+str(int(MLOceanEnsemble.t))+".npy")
+
+    Hx, Hy = MLOceanEnsemble.obsLoc2obsIdx(obs_xs[0], obs_ys[0])
+    obs = [true_eta[Hy,Hx], true_hu[Hy,Hx], true_hv[Hy,Hx]] + np.random.normal(0,R)
+    
+    ML_K = MLEnKF.assimilate(MLOceanEnsemble, obs, obs_xs[0], obs_ys[0], R, 
+                                    r=r, obs_var=slice(1,3), relax_factor=relax_factor, 
+                                    min_localisation_level=min_location_level)
+
+
+    # precomp_GC = []
+    # for obs_x, obs_y in zip(obs_xs, obs_ys):
+    #     precomp_GC.append( MLEnKF.GCweights(obs_x, obs_y, r) )
+    
+    # #####################
+    # # Data assimilation
+    # while MLOceanEnsemble.t < T_da:
+    #     # Forward step
+    #     MLOceanEnsemble.stepToObservation(MLOceanEnsemble.t + da_timestep)
+
+    #     # DA step
+    #     true_eta, true_hu, true_hv = np.load("tmpTruth/truth_"+str(int(MLOceanEnsemble.t))+".npy")
+
+    #     for h, [obs_x, obs_y] in enumerate(zip(obs_xs, obs_ys)):
+    #         Hx, Hy = MLOceanEnsemble.obsLoc2obsIdx(obs_x, obs_y)
+    #         obs = [true_eta[Hy,Hx], true_hu[Hy,Hx], true_hv[Hy,Hx]] + np.random.normal(0,R)
+            
+    #         ML_K = MLEnKF.assimilate(MLOceanEnsemble, obs, obs_x, obs_y, R, 
+    #                                 r=r, obs_var=slice(1,3), relax_factor=relax_factor, 
+    #                                 min_localisation_level=min_location_level,
+    #                                 precomp_GC=precomp_GC[h])
+            
+    # Saving results
+    os.makedirs("tmpMLGain", exist_ok=True)
+    np.save("tmpMLGain/"+datetime.datetime.now().strftime("%Y-%m-%dT%H_%M_%S"), ML_K.reshape(3,MLOceanEnsemble.nys[-1],MLOceanEnsemble.nxs[-1],ML_K.shape[-1]))
+
+    # Crash to let OS do the gc
+    os._exit(0)
+    
+
+
+# %%
