fc6cec57bb748926adccf4f3320954f4a5c59e64
FlorianBeiser <florian.beiser@sintef.no>
2023-09-19 14:35:47 +0200

    Variance level analysis for doublejet and ensemble sizes

--- scripts/run_script.sh
+++ scripts/run_script.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-python sleep.py --T 5200
+python DoubleJetMLDA.py -ls 7 8 -Ne 37 27
 
-python DoubleJetSLDA.py
+python DoubleJetMLDA.py -ls 6 7 8 -Ne 12 22 24
 
-python DoubleJetMLDA.py
+python DoubleJetSLDA.py -L 8 -Ne 50
+
+python DoubleJetSLDA.py -L 7 -Ne 118
+
+python DoubleJetSLDA.py -L 6 -Ne 170
